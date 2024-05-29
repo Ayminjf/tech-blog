98c7ac4ee5aeedeebca0cd0654a36ecb305d9577
Aymin <ayminjf@gmail.com>
2024-05-29 17:36:36 +0330

    feat(assets): add colors

---
+++ lib/constants/my_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class solidColors {
+  static const Color primary = Color(0xFF440457);
+  static const Color colorTitle = Color(0xFF286BB8);
+  static const Color textTile = Color(0xFF000000);
+  static const Color scaffoldBg = Color(0xFFFFFFFF);
+  static const Color lightText = Color(0xFFFFFFFF);
+  static const Color selectedPodCast = Color(0xFFFF8B1A);
+  static const Color submitArticle = Color(0xFFD1D1D1);
+  static const Color submitPodCast = Color(0xFFF6F6F6);
+  static const Color subText = Color(0xFFC5C5C5);
+}
+
+class gradiantColors {
+  static const List<Color> bottomNav = [Color(0xFF19005E), Color(0xFF440457)];
+  static const List<Color> tags = [Color(0xFF000000), Color(0xFF3F3F3F)];
+}
